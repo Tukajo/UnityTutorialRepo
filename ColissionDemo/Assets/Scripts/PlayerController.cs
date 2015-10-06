1292374b96eb3a21711ddb2783d51fd5199f7f6f
Timothy J. Frisch <tjfrisch@gmail.com>
2015-10-05 21:30:29 -0500

    Basic movement & Collection
    Collection items.

---
+++ ColissionDemo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    //Speed scale, editible property in inspector.
+    public float speed;
+
+    private Rigidbody playerBody;
+
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+    
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        playerBody.AddForce(movement*speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+}
