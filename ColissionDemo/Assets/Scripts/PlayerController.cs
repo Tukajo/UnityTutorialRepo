facf1fda891f261b6a45bcbba36d34a0e3605218
Timothy J. Frisch <tjfrisch@gmail.com>
2015-10-05 21:47:41 -0500

    Finished tutorial for ball roll
    Finished the first tutorial.

--- ColissionDemo/Assets/Scripts/PlayerController.cs
+++ ColissionDemo/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     //Speed scale, editible property in inspector.
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody playerBody;
+    private int count = 0;
 
     void Start()
     {
         playerBody = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -28,8 +35,18 @@ public class PlayerController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Pick Up"))
         {
+            count++;
             other.gameObject.SetActive(false);
+            SetCountText();
         }
     }
 
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
 }
